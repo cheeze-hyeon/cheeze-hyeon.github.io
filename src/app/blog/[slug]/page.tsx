4286f17c7d2379940601962d2a64eb088d5011a7
최주현 <wngus0223@naver.com>
2025-02-17 23:12:48 +0900

    fix: Promise

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,9 +4,9 @@ import { notFound } from "next/navigation";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 interface BlogPostProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
 export default async function BlogPost({ params }: BlogPostProps) {
