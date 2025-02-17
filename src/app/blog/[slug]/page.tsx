8120fa30c91e2995a19883ea95b8c1936889924d
최주현 <wngus0223@naver.com>
2025-02-17 22:49:50 +0900

    fix: type error

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,11 +3,16 @@ import path from "path";
 import { notFound } from "next/navigation";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
-export default async function BlogPost({ params }: any) {
+interface BlogPostProps {
+  params: { slug: string };
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
   const { slug } = await params;
 
   const filePath = path.join(process.cwd(), "src/posts", `${slug}.mdx`);
   const decodedSlug = decodeURIComponent(filePath);
+
   if (!fs.existsSync(decodedSlug)) {
     return notFound(); // 파일이 없으면 404 처리
   }
