e181a4c2357e1fd4d92703a380d4a2834a5092fb
최주현 <wngus0223@naver.com>
2025-03-10 20:21:10 +0900

    feat: 마크다운 css 적용

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,14 +4,30 @@ import { notFound } from "next/navigation";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
 interface BlogPostProps {
-  params: Promise<{
-    slug: string;
-  }>;
+  params: Promise<{ slug: string }>;
 }
+
+// 각 태그에 적용할 커스텀 스타일 컴포넌트
+const customComponents = {
+  h1: (props: any) => (
+    <h1 className="text-4xl font-bold text-orange-500" {...props} />
+  ),
+  h2: (props: any) => (
+    <h2 className="text-3xl font-semibold text-blue-500" {...props} />
+  ),
+  p: (props: any) => (
+    <p className="text-lg leading-7 text-gray-700" {...props} />
+  ),
+  ul: (props: any) => (
+    <ul className="list-disc pl-5 text-gray-700" {...props} />
+  ),
+  ol: (props: any) => (
+    <ol className="list-decimal pl-5 text-gray-700" {...props} />
+  ),
+};
 
 export default async function BlogPost({ params }: BlogPostProps) {
   const { slug } = await params;
-
   const filePath = path.join(process.cwd(), "src/posts", `${slug}.mdx`);
   const decodedSlug = decodeURIComponent(filePath);
 
@@ -22,8 +38,8 @@ export default async function BlogPost({ params }: BlogPostProps) {
   const mdxContent = fs.readFileSync(decodedSlug, "utf-8");
 
   return (
-    <div>
-      <MDXRemote source={mdxContent} />
+    <div style={{ maxWidth: "800px", margin: "auto", padding: "20px" }}>
+      <MDXRemote source={mdxContent} components={customComponents} />
     </div>
   );
 }
